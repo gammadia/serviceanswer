c3a8b3471eaba2cd372714fb4244992f6dcba250
Joël Poulin <joel@voilab.org>
2014-05-16 19:08:22 +0200

    - improve message system

--- lib/Traits/Base.php
+++ lib/Traits/Base.php
@@ -13,6 +13,11 @@ trait Base {
         return $this->body;
     }
 
+    public function setBody($body) {
+        $this->body = $body;
+        return $this;
+    }
+
     public function getMessages() {
         return $this->messages;
     }
@@ -23,5 +28,25 @@ trait Base {
 
     public function getErrorCode() {
         return $this->errorCode;
+    }
+
+    public function setErrorCode($code) {
+        $this->errorCode = $code;
+        return $this;
+    }
+
+    public function setPublicMessage($message) {
+        $this->messages['public'] = $message;
+        return $this;
+    }
+
+    public function setDeveloperMessage($message) {
+        $this->messages['dev'] = $message;
+        return $this;
+    }
+
+    public function setEmptyBodyMessage($message) {
+        $this->messages['empty'] = $message;
+        return $this;
     }
 }
